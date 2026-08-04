2eabbd585b81ca6f149c0713355b7dd19a5c48fd
あなたの名前 <>
2026-08-04 10:33:29 +0900

    Add microphone permissions, entitlements, and GitHub Actions build workflow

--- browser/Reynard/AppDelegate.swift
+++ browser/Reynard/AppDelegate.swift
@@ -5,13 +5,23 @@
 //  Created by Minh Ton on 1/2/26.
 //
 
+import AVFoundation
 import UIKit
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         JITController.shared.startBackgroundAudioIfNeeded()
+        requestMicrophonePermission()
         return true
+    }
+    
+    private func requestMicrophonePermission() {
+        if #available(iOS 17.0, *) {
+            AVAudioApplication.requestRecordPermission { _ in }
+        } else {
+            AVAudioSession.sharedInstance().requestRecordPermission { _ in }
+        }
     }
     
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
